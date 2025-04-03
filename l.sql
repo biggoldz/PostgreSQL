df7029e876ad3a7a8e31f87faa4f381dd536f8d1
biggoldz <133200464+biggoldz@users.noreply.github.com>
2025-04-03 16:46:45 +0300

    Add files via upload

---
+++ l.sql
@@ -0,0 +1,11 @@
+-- Скрипт для поиска параметров конфигурации по начальным буквам
+\prompt 'Введите начальные буквы имени параметра (можно с %): ' param_pattern
+
+-- Если пользователь не ввел символ %, добавляем его автоматически
+\set quoted_pattern '\'%' :'param_pattern' '%\''
+
+-- Выполняем поиск с подстановкой шаблона
+SELECT * FROM pg_file_settings 
+WHERE name LIKE :quoted_pattern
+ORDER BY name;
+
