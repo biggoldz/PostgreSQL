7e73836d68bc8e336df4255144247fe75e96f09f
biggoldz <133200464+biggoldz@users.noreply.github.com>
2025-05-21 11:47:42 +0300

    Add files via upload

---
+++ grant_user_schema_table.sql
@@ -0,0 +1,5 @@
+\prompt 'Введите имя учетной записи: ' user_name
+\prompt 'Введите имя схемы данных: ' schema_name
+\prompt 'Введите имя таблицы: ' tab_name
+SELECT table_catalog, table_schema, table_name, privilege_type, is_grantable FROM information_schema.role_table_grants WHERE grantee=:'user_name' and table_schema=:'schema_name'and AND table_name =:'tab_name';
+
