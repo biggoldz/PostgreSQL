5970e01df13dadb981681056eaee09dbf8e6c8cf
biggoldz <133200464+biggoldz@users.noreply.github.com>
2025-08-19 13:20:36 +0300

    Update active_find.sql

--- active_find.sql
+++ active_find.sql
@@ -1,10 +1,9 @@
-*/---[ RECORD 1 ]------------------------------
-secs     | 5.845579
-pid      | 14089
-database | tpcc
-state    | active
-query    | autovacuum: ANALYZE public.stock
-*/
+---------[ RECORD 1 ]------------------------------
+--secs     | 5.845579
+--pid      | 14089
+--database | tpcc
+--state    | active
+--query    | autovacuum: ANALYZE public.stock
 select extract(epoch from (clock_timestamp()-xact_start)) secs, pid, 
 datname database, state, query 
 from pg_stat_activity 
